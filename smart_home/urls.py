c0ad61da9448941a665467f0463e3e8189639e03
jack ogina <jackogina60@gmail.com>
2018-07-14 18:50:35 +0300

    app init scripts

--- smart_home/urls.py
+++ smart_home/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^',include(('restLogin.urls','restLogin'),namespace="rest-login")),
+    url(r'^',include(('rooms.urls','rooms'),namespace="rooms")),
 ]
