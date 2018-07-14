58cbed4486250890aabf3b9ffccf956459b63065
jack ogina <jackogina60@gmail.com>
2018-07-14 19:54:48 +0300

    nebular theme
    components,directives,layouts, pipes and styles

---
+++ front-end/src/app/@theme/components/header/header.component.ts
@@ -0,0 +1,61 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
+import { NbMenuService, NbSidebarService } from '@nebular/theme';
+import { UserService } from '../../../@core/data/users.service';
+import { AnalyticsService } from '../../../@core/utils/analytics.service';
+
+@Component({
+  selector: 'ngx-header',
+  styleUrls: ['./header.component.scss'],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent implements OnInit {
+
+
+  @Input() position = 'normal';
+
+  user: any;
+
+  userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
+
+  constructor(private sidebarService: NbSidebarService,
+              private menuService: NbMenuService,
+              private userService: UserService,
+              private analyticsService: AnalyticsService,
+              private authService: NbAuthService) {
+
+    this.authService.onTokenChange()
+    .subscribe((token: NbAuthJWTToken) => {
+
+      if (token.isValid()) {
+        this.user = token.getPayload(); // here we receive a payload from the token and assigne it to our `user` variable 
+        console.log(this.user)
+      }
+
+    });
+
+  }
+
+  ngOnInit() {
+    this.userService.getUsers()
+      .subscribe((users: any) => this.user = users.nick);
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+    return false;
+  }
+
+  toggleSettings(): boolean {
+    this.sidebarService.toggle(false, 'settings-sidebar');
+    return false;
+  }
+
+  goToHome() {
+    this.menuService.navigateHome();
+  }
+
+  startSearch() {
+    this.analyticsService.trackEvent('startSearch');
+  }
+}
---
+++ front-end/src/app/@theme/components/theme-switcher/themes-switcher-list/themes-switcher-list.component.ts
@@ -0,0 +1,52 @@
+import {Component, Input} from '@angular/core';
+import { NbThemeService, NbPopoverDirective } from '@nebular/theme';
+import { AnalyticsService } from '../../../../@core/utils/analytics.service';
+import { NbJSThemeOptions } from '@nebular/theme/services/js-themes/theme.options';
+
+@Component({
+  selector: 'ngx-theme-switcher-list',
+  template: `
+    <ul class="themes-switcher-list">
+      <li class="themes-switcher-item"
+          *ngFor="let theme of themes"
+          (click)="onToggleTheme(theme.key)">
+        <i class="nb-drop" [ngClass]="'drop-icon-' + theme.key"></i>
+        <span>{{ theme.title }}</span>
+      </li>
+    </ul>
+  `,
+  styleUrls: ['./theme-switcher-list.component.scss'],
+})
+export class ThemeSwitcherListComponent {
+
+  @Input() popover: NbPopoverDirective;
+
+  theme: NbJSThemeOptions;
+
+  themes = [
+    {
+      title: 'Corporate',
+      key: 'corporate',
+    },
+    {
+      title: 'Light',
+      key: 'default',
+    },
+    {
+      title: 'Cosmic',
+      key: 'cosmic',
+    },
+  
+  ];
+
+  constructor(
+    private themeService: NbThemeService,
+    private analyticsService: AnalyticsService,
+  ) {}
+
+  onToggleTheme(themeKey: string) {
+    this.themeService.changeTheme(themeKey);
+    this.analyticsService.trackEvent('switchTheme');
+    this.popover.hide();
+  }
+}
