c9e8a4e23d5e2b6c93605d7bae2f04d098c133d9
jack ogina <jackogina60@gmail.com>
2018-07-14 18:51:35 +0300

    admin register models

---
+++ rooms/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from  .models import Device,DeviceTypes
+
+admin.site.register(
+    
+        Device,
+        list_display=["name","room","pin_number"]
+
+)
+
+admin.site.register(
+        DeviceTypes,
+        list_display=["name","iconclass","style_type"]
+        )
