dc1afcf607e6b36db9ac47a787a3b3c62c7ba0c7
jack ogina <jackogina60@gmail.com>
2018-07-14 19:30:15 +0300

    common app functions
    get_home_data - home architecture data from the json file
    get_rooms - returns the rooms in the home
    get_nb_icons - returns a list of nebular icons

---
+++ rooms/utils.py
@@ -0,0 +1,16 @@
+import json
+
+
+def get_home_data():
+    with open('rooms/home_architecture.json','r') as rm:
+        data=json.load(rm)
+    return data
+
+def get_rooms():
+    data=get_home_data()
+    return [i['name']['text'] for i in data["rooms"]]
+
+def get_nb_icons():
+    with open('rooms/nb-icons.txt') as ic:
+        icons_list=ic.read().split('\n')
+    return icons_list
