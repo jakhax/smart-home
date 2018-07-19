e19b51d69f0caa2b880740e4c1eca52886009120
Lucas <plucaslambert@gmail.com>
2018-07-19 09:02:01 +0300

    FILES AND CODE FOR ARDUINO HARDWARE:
    	Web client added
    	Laser trip wire added
    	Sound detector added
    	Current sensor added
    	Flame detector added
    
    	Due to analog pin restrictions on the rby-pi we needed to post the arduino data through the ethenet shield as text file data.
    	On posting the data we flush the sd card and start storing the data again.
    	We POST the data to the server every 5 minutes

---
+++ arduino_files/lcd code/I2C_LCD_TUTORIAL.ino
@@ -0,0 +1,34 @@
+/* www.learningbuz.com */
+/*Impport following Libraries*/
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+//I2C pins declaration
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+
+void setup() 
+{
+
+lcd.begin(16,2);//Defining 16 columns and 2 rows of lcd display
+lcd.backlight();//To Power ON the back light
+//lcd.backlight();// To Power OFF the back light
+
+}
+
+void loop() 
+{
+//Write your code
+lcd.setCursor(0,0); //Defining positon to write from first row,first column .
+lcd.print(" Tech Maker "); //You can write 16 Characters per line .
+delay(1000);//Delay used to give a dynamic effect
+lcd.setCursor(0,1);  //Defining positon to write from second row,first column .
+lcd.print("Like | Share");
+delay(8000); 
+
+lcd.clear();//Clean the screen
+lcd.setCursor(0,0); 
+lcd.print(" SUBSCRIBE ");
+lcd.setCursor(0,1);
+lcd.print(" TECH MAKER ");
+delay(8000); 
+}
+
