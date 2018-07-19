8699c7d535730804dc52c78911a9b825514f01af
Sarah-Marion <devsarahmarion@gmail.com>
2018-07-19 17:28:20 +0300

    Add serializers tests

--- restLogin/tests/test_serializers.py
+++ restLogin/tests/test_serializers.py
@@ -1,14 +1,4 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-from .views import LoginUserView
-
 import rest_framework
 import django
 from rest_framework_jwt.compat import get_user_model
@@ -25,23 +15,6 @@ drf3 = rest_framework.VERSION >= StrictVersion('3.0.0')
 
 
 User = get_user_model()
-
-# Create your tests here.
-class LoginUser(APITestCase):
-    # urlpatterns = [
-    #     path((r'^',include(('restLogin.urls','restLogin'),namespace="rest-login")),
-    #     ]
-
-    def test_create_account(self):
-        """
-        Ensure we can create a new account object.
-        """
-        # url = reverse('account-list')
-        data = {'name': 'Washere'}
-        response = self.client.post( data, format='json')
-        self.assertTrue(response.status_code, status.HTTP_201_CREATED)
-        # self.assertEqual(Account.objects.count(), 1)
-        # self.assertEqual(Account.objects.get().name, 'Washere')
 
 class JSONWebTokenSerializerTests(TestCase):
     def setUp(self):
